ede73606a515ad29877a4a8815a3a8dbd68df402
JairDavid <jairvasmar@gmail.com>
2021-11-28 14:39:47 -0600

    frst commit

--- src/pages/DashboardProjects.jsx
+++ src/pages/DashboardProjects.jsx
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import CssBaseline from '@mui/material/CssBaseline';
 import Divider from '@mui/material/Divider';
+import Card from '@mui/material/Card';
 import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
@@ -30,7 +31,6 @@ function ResponsiveDrawer(props) {
     const drawer = (
         <div>
             <Toolbar />
-            <Divider />
             <List>
                 {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
                     <ListItem button key={text}>
@@ -41,7 +41,6 @@ function ResponsiveDrawer(props) {
                     </ListItem>
                 ))}
             </List>
-            <Divider />
             <List>
                 {['All mail', 'Trash', 'Spam'].map((text, index) => (
                     <ListItem button key={text}>
@@ -58,39 +57,17 @@ function ResponsiveDrawer(props) {
     const container = window !== undefined ? () => window().document.body : undefined;
 
     return (
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex' }} style={{ backgroundColor: '#F3F5F9' }} >
             <CssBaseline />
-            <AppBar
-                position="fixed"
-                sx={{
-                    width: { sm: `calc(100% - ${drawerWidth}px)` },
-                    ml: { sm: `${drawerWidth}px` },
-                }}
-            >
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{ mr: 2, display: { sm: 'none' } }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" noWrap component="div">
-                        Responsive drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>
             <Box
-                component="nav"
+
                 sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
                 aria-label="mailbox folders"
             >
                 {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
                 <Drawer
+                    style={{ borderRadius: 20 }}
                     container={container}
-                    variant="temporary"
                     open={mobileOpen}
                     onClose={handleDrawerToggle}
                     ModalProps={{
@@ -104,6 +81,7 @@ function ResponsiveDrawer(props) {
                     {drawer}
                 </Drawer>
                 <Drawer
+                    style={{ borderRadius: 20 }}
                     variant="permanent"
                     sx={{
                         display: { xs: 'none', sm: 'block' },
@@ -116,36 +94,57 @@ function ResponsiveDrawer(props) {
             </Box>
             <Box
                 component="main"
-                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+                sx={{ flexGrow: 1, p: 2, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
             >
-                <Toolbar />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-                    sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-                    eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-                    neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-                    tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-                    sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-                    tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-                    gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-                    et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-                    tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-                    eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-                    posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
+                <AppBar
+                    style={{ borderRadius: 10, padding: 8, backgroundColor: '#FFFEFF', boxShadow: 'box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;' }}
+                    position="sticky"
+                >
+                    <Toolbar  >
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            sx={{ mr: 2, display: { sm: 'none' } }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" noWrap component="div" style={{ color: '#333348' }}>
+                            Responsive drawer
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Card style={{ boxShadow: 'box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;', marginTop: 30, padding: 20, borderRadius: 20 }}>
+                    <Typography paragraph>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                        enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                        imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                        Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                        Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                        adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                        nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                        leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                        feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                        consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                        sapien faucibus et molestie ac.
+                    </Typography>
+                    <Typography paragraph>
+                        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+                        eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+                        neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+                        tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+                        sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+                        tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+                        gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+                        et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+                        tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+                        eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+                        posuere sollicitudin aliquam ultrices sagittis orci a.
+                    </Typography>
+                </Card>
+
             </Box>
         </Box>
     );
